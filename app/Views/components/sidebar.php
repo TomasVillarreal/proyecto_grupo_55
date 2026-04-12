00fa8f82ec6c1cf2f65033b3e22331147886af58
Tomas Villarreal <tomiavillarreal@gmail.com>
2026-04-12 20:08:49 -0300

    creacion del sidebar funcional, y creacion de un form de prueba en el create medicamentos

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -1,36 +1,293 @@
-<div class="sidebar sidebar-narrow-unfoldable border-end">
-  <div class="sidebar-header border-bottom">
-    <div class="sidebar-brand">CUI</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sidebar Menu</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="carousel.css">
+
+
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap");
+:root {
+  --header-height: 3rem;
+  --nav-width: 68px;
+  --first-color: #031530;
+  --first-color-light: #afa5d9;
+  --white-color: #f7f6fb;
+  --body-font: "Nunito", sans-serif;
+  --normal-font-size: 1rem;
+  --z-fixed: 100;
+}
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+body {
+  position: relative;
+  margin: var(--header-height) 0 0 0;
+  padding: 0 1rem;
+  font-family: var(--body-font);
+  font-size: var(--normal-font-size);
+  transition: 0.5s;
+  background: #f8f9fa;
+}
+
+a {
+  text-decoration: none;
+}
+
+.header {
+  width: 100%;
+  height: var(--header-height);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+  background-color: var(--white-color);
+
+  z-index: var(--z-fixed);
+  transition: 0.5s;
+  box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 16%);
+  -webkit-box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 16%);
+  -moz-box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 16%);
+}
+
+.dropdown-menu {
+  box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 24%);
+  -webkit-box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 24%);
+  -moz-box-shadow: 1px 1px 17px -4px rgb(0 0 0 / 24%);
+  border-radius: 10px;
+  margin: 0;
+  padding: 0;
+}
+
+.dropdown-item {
+  padding: 7px 15px;
+}
+
+.dropdown-item:hover {
+  background-color: #03153056;
+  color: var(--white-color);
+  border-radius: 10px;
+}
+
+.header_toggle {
+  color: var(--first-color);
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+
+.header_img {
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.header_img img {
+  width: 40px;
+}
+
+.l-navbar {
+  position: fixed;
+  top: 0;
+  left: -30%;
+  width: var(--nav-width);
+  height: 100vh;
+  background-color: var(--first-color);
+  padding: 0.5rem 1rem 0 0;
+  transition: 0.5s;
+  z-index: var(--z-fixed);
+}
+
+.nav {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow: hidden;
+}
+
+.nav_logo,
+.nav_link {
+  display: grid;
+  grid-template-columns: max-content max-content;
+  align-items: center;
+  column-gap: 1rem;
+  padding: 0.5rem 0 0.5rem 1.5rem;
+}
+
+.nav_logo {
+  margin-bottom: 2rem;
+}
+
+.nav_logo-icon {
+  font-size: 1.25rem;
+  color: var(--white-color);
+}
+
+.nav_logo-name {
+  color: var(--white-color);
+  font-weight: 700;
+}
+
+.nav_link {
+  position: relative;
+  color: var(--first-color-light);
+  margin-bottom: 1.5rem;
+  transition: 0.3s;
+}
+
+.nav_link:hover {
+  color: var(--white-color);
+}
+
+.nav_icon {
+  font-size: 1.25rem;
+}
+
+.sidebarshow {
+  left: 0;
+}
+
+.body-pd {
+  padding-left: calc(var(--nav-width) + 1rem);
+}
+
+.active {
+  color: var(--white-color);
+}
+
+.active::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  width: 2px;
+  height: 32px;
+  background-color: var(--white-color);
+}
+
+.height-100 {
+  height: 100vh;
+  padding-top: 25px;
+}
+
+.btn-group button {
+  outline: none;
+  box-shadow: none;
+  border: none;
+  background: transparent;
+}
+
+.dropdown-toggle::after {
+  display: none;
+}
+
+@media screen and (min-width: 768px) {
+  body {
+    margin: calc(var(--header-height) + 1rem) 0 0 0;
+    padding-left: calc(var(--nav-width) + 1.5rem);
+  }
+
+  .header {
+    height: calc(var(--header-height) + 1rem);
+    padding: 0 2rem 0 calc(var(--nav-width) + 2rem);
+  }
+
+  .header_img {
+    width: 40px;
+    height: 40px;
+  }
+
+  .header_img img {
+    width: 45px;
+  }
+
+  .l-navbar {
+    left: 0;
+    padding: 1rem 1rem 0 0;
+  }
+
+  .sidebarshow {
+    width: calc(var(--nav-width) + 200px);
+  }
+
+  .body-pd {
+    padding-left: calc(var(--nav-width) + 232px);
+  }
+}
+  </style>  
+</head>
+
+<body id="body-pd">
+  <header class="header" id="header">
+    <div class="header_toggle"> <i class='bi bi-list' id="header-toggle"></i> </div>
+  </header>
+  <div class="l-navbar" id="nav-bar">
+    <nav class="nav">
+      <div> 
+        <a href="#" class="nav_logo"> <i class='bi bi-hospital nav_logo-icon'></i><span class="nav_logo-name">Clinicks</span> </a>
+        <div class="nav_list"> 
+            <a href="#" class="nav_link active"> <i class='bi bi-plus-circle nav_icon'></i> <span class="nav_name">Agregar medicamento</span> </a> 
+            <a href="#" class="nav_link"> <i class='bi bi-pencil-square nav_icon'></i> <span class="nav_name">Modificar medicamento</span> </a> 
+            <a href="#" class="nav_link"> <i class='bi bi-file-x nav_icon'></i> <span class="nav_name">Eliminar medicamento</span> </a> 
+        </div>
+      </div> 
+    </nav>
   </div>
-  <ul class="sidebar-nav">
-    <li class="nav-title">Nav Title</li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="nav-icon cil-speedometer"></i> Nav item
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="nav-icon cil-speedometer"></i> With badge
-        <span class="badge bg-primary ms-auto">NEW</span>
-      </a>
-    </li>
-    <li class="nav-item nav-group show">
-      <a class="nav-link nav-group-toggle" href="#">
-        <i class="nav-icon cil-puzzle"></i> Nav dropdown
-      </a>
-      <ul class="nav-group-items">
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span class="nav-icon"><span class="nav-icon-bullet"></span></span> Nav dropdown item
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span class="nav-icon"><span class="nav-icon-bullet"></span></span> Nav dropdown item
-          </a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</div>
+
+
+  <!--Container Main end-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.bundle.min.js"></script>
+  <script src="main.js"></script>
+  <script>
+    document.addEventListener("DOMContentLoaded", function (event) {
+        const showNavbar = (toggleId, navId, bodyId, headerId) => {
+            const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId);
+            // Validate that all variables exist
+            if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener("click", () => {
+                // show navbar
+                nav.classList.toggle("sidebarshow");
+                // change icon
+                toggle.classList.toggle("bi-x");
+                // add padding to body
+                bodypd.classList.toggle("body-pd");
+                // add padding to header
+                headerpd.classList.toggle("body-pd");
+            });
+            }
+        };
+        showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+        /*===== LINK ACTIVE =====*/
+        const linkColor = document.querySelectorAll(".nav_link");
+
+        function colorLink() {
+            if (linkColor) {
+            linkColor.forEach((l) => l.classList.remove("active"));
+            this.classList.add("active");
+            }
+        }
+        linkColor.forEach((l) => l.addEventListener("click", colorLink));
+        // Your code to run since DOM is loaded and ready
+        });
+  </script>
+</body>
+
+</html>
