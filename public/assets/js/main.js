8f252cd402e00340eac0b62200c64445e34216aa
Tomas Villarreal <tomiavillarreal@gmail.com>
2026-04-18 19:12:17 -0300

    coreccion error del sidebar que no marcaba la pagina elegida, ahora si lo hace

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,35 +1,35 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+
   const showNavbar = (toggleId, navId, bodyId, headerId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
-    // Validate that all variables exist
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+    const bodypd = document.getElementById(bodyId);
+    const headerpd = document.getElementById(headerId);
+
     if (toggle && nav && bodypd && headerpd) {
       toggle.addEventListener("click", () => {
-        // show navbar
         nav.classList.toggle("sidebarshow");
-        // change icon
         toggle.classList.toggle("bx-x");
-        // add padding to body
         bodypd.classList.toggle("body-pd");
-        // add padding to header
         headerpd.classList.toggle("body-pd");
       });
     }
   };
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
 
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
+  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+  /* ACTIVE SEGÚN URL */
+  const links = document.querySelectorAll(".nav_link");
+  const currentUrl = window.location.href;
+
+  links.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.href === currentUrl) {
+      link.classList.add("active");
     }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
-  // Your code to run since DOM is loaded and ready
+  });
+
 });
 
 
