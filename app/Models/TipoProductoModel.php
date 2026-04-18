a319cab5857c8d89e2eba750fe6fe24bd71081b8
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-04-18 11:40:36 -0300

    Mejoré el metodo de tipo producto, aunque no había mucho que hacer

--- app/Models/TipoProductoModel.php
+++ app/Models/TipoProductoModel.php
@@ -8,10 +8,31 @@ class TipoProductoModel extends Model
 {
     protected $table = 'tipo_producto'; //El nombre de nuestra tabla en bd
     protected $primaryKey = 'id_tipo_producto';
+    protected $allowedFields = ['nombre_tipo_producto']; //La columna de la tabla
+    protected $useTimestamps = false;
+    protected $returnType = 'object';
 
-    protected $allowedFields = [//La columna de la tabla
-        'nombre_tipo_producto'
-    ];
+    /*Se crea un método para obtener todos los tipos de productos ordenados alfabeticamente,
+    lo cual va a ser útil para cargar en el formulario*/
+    
+    public function obtenerTiposProductos(): array
+    {
+        return $this->orderby('nombre_tipo_producto','ASC')->findAll();
+    }
 
+    /*Posible método a implementar, lo que busca es asociar un ID a un tipo en especifico
+    lo que en teoría facilitaría la carga a los dropdowns pero no sé si conviene por si en
+    un futuro hay modificaciones en los Ids, lo dejo igual por las dudas.
+
+    ´public function obtenerParaDropdown(): array
+    {
+        $tipos = $this->orderBy('nombre_tipo_producto', 'ASC')->findAll();
+        $options = [];
+        foreach ($tipos as $tipo) {
+            $options[$tipo->id_tipo_producto] = $tipo->nombre_tipo_producto;
+        }
+        return $options;
+    }
+    */
 }
 
