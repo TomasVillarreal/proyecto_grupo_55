5b0c3b187abbd741c5052c2a908c825c0ca99de2
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-04-18 11:56:12 -0300

    Mejoras en el modelo de las medidas de los productos, en teoría no se necesitan más agregaciones

--- app/Models/MedidaProductoModel.php
+++ app/Models/MedidaProductoModel.php
@@ -7,9 +7,23 @@ use CodeIgniter\Model;
 class MedidaProductoModel extends Model
 {
     protected $table = 'medida_producto';//El nombre de la tabla en nuestra bd
-    protected $primaryKey = 'id_medida_producto';
+    protected $primaryKey = 'id_medida_producto'; //Identificador único
+    protected $allowedFields = ['nombre_medida']; //Columna de la tabla
+    protected $useTimestamps = false; //Para evitar guardar y asignar fechas automaticamente
+    protected $returnType = 'object'; //Formato de dato a devolver
 
-    protected $allowedFields = [//La única fila editable
-        'nombre_medida'
-    ];
+    /*Se crea un método para obtener todos las medidas de las dosis de los productos ordenados alfabeticamente,
+    lo cual va a ser útil para cargar en el formulario.
+    Asocia a cada ID su nombre de medida y es más fácil llamarlo en los formualrios
+    ahorrando lógica en un controlador*/
+    
+    public function obtenerParaDropdown(): array
+    {
+        $medidas = $this->orderBy('nombre_medida', 'ASC')->findAll();
+        $opciones_medidas = [];
+        foreach ($medidas as $medida) {
+            $opciones_medidas[$medida->id_medida_producto] = $medida->nombre_medida;
+        }
+        return $opciones_medidas;
+    }
 }
