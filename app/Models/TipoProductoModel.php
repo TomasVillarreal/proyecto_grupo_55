951b3c1b687d800e87261804b0833759137e5840
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-04-18 11:54:57 -0300

    Hice unas mejoras en el modelo, en teoría ya está listo

--- app/Models/TipoProductoModel.php
+++ app/Models/TipoProductoModel.php
@@ -7,32 +7,24 @@ use CodeIgniter\Model;
 class TipoProductoModel extends Model
 {
     protected $table = 'tipo_producto'; //El nombre de nuestra tabla en bd
-    protected $primaryKey = 'id_tipo_producto';
+    protected $primaryKey = 'id_tipo_producto'; //Identificador único
     protected $allowedFields = ['nombre_tipo_producto']; //La columna de la tabla
-    protected $useTimestamps = false;
-    protected $returnType = 'object';
+    protected $useTimestamps = false; //Para evitar asignaciones y guardados de fecha automaticamente.
+    protected $returnType = 'object'; //Formato de dato a devolver
 
     /*Se crea un método para obtener todos los tipos de productos ordenados alfabeticamente,
-    lo cual va a ser útil para cargar en el formulario*/
-    
-    public function obtenerTiposProductos(): array
-    {
-        return $this->orderby('nombre_tipo_producto','ASC')->findAll();
-    }
+    lo cual va a ser útil para cargar en el formulario.
+    Asocia a cada ID su tipo de producto y es más fácil llamarlo en los formualrios
+    ahorrando lógica en un controlador*/
 
-    /*Posible método a implementar, lo que busca es asociar un ID a un tipo en especifico
-    lo que en teoría facilitaría la carga a los dropdowns pero no sé si conviene por si en
-    un futuro hay modificaciones en los Ids, lo dejo igual por las dudas.
-
-    ´public function obtenerParaDropdown(): array
+    public function obtenerParaDropdown(): array
     {
         $tipos = $this->orderBy('nombre_tipo_producto', 'ASC')->findAll();
-        $options = [];
+        $opciones_tipos = [];
         foreach ($tipos as $tipo) {
-            $options[$tipo->id_tipo_producto] = $tipo->nombre_tipo_producto;
+            $opciones_tipos[$tipo->id_tipo_producto] = $tipo->nombre_tipo_producto;
         }
-        return $options;
+        return $opciones_tipos;
     }
-    */
 }
 
