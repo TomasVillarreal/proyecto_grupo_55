ce9635c30c59e595b048d3c9de22710eab2d0cf4
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-04-20 14:28:27 -0300

    Hice algunas modificaciones necesarias en el archivo de las rutas para poder ejecutar el post de los formularios

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'MedicamentosController::create');
+$routes->post('medicamentos/alta', 'MedicamentosController::altaMedicamento');//Ruta con el POST para la cracion de medicamentos y/o productos farm.
 $routes->get('/update', 'MedicamentosController::update');
 $routes->get('/delete', 'MedicamentosController::delete');
 
