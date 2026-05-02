50c850e22fd07e363fdbd3168fac6c09e4d3fd35
Tomas Villarreal <tomiavillarreal@gmail.com>
2026-05-02 14:28:46 -0300

    creación del service para detalle pedido, y del metodo que llama a la funcion del modelo y que devuelve los detalles de un pedido particular

---
+++ app/Services/DetallePedidoService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\DetallePedidoModel;
+
+class DetallePedidoService
+{
+    //Variable a utilizar que hace referncia al modelo
+    protected $detalleModel;
+
+    /*Creacion del constructor para evitar llamar al modelo en cada funcion*/
+    public function __construct()
+    {
+        $this->detalleModel = model(DetallePedidoService::class);//Se reconoce e instancia la clase
+    }
+
+    /*Metodo para obtener los detalles de un pedido particular, 
+    haciendo uso del metodo del modelo, y devolviendolo*/
+    public function obtenerDetallesPedido(int $id_pedido): array
+    {
+        $detalles = $this->detalleModel->obtenerDetallesPedido($id_pedido);
+
+        if (!$detalles) {
+            throw new \Exception("Pedido no encontrado");
+        }
+
+        return $detalles;
+    }
+}
