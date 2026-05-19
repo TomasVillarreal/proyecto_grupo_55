85a4838bc132c773c5f4da41395da224718fe166
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-05-19 15:48:37 -0300

    Se crea la rama de Usuario para crear todo lo referido al usuario. Además, se crea un model de usuario que contiene su información y de momento un método para obtener la información de un usuario con su id pasado por parametro

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsuarioModel extends Model{
+    protected $table = 'usuario'; //El nombre de la tabla
+    protected $primaryKey = 'id_usuario'; //Clave primaria el id de usuario
+    protected $allowedFields = ['dni_usuario','nombre_usuario','apellido_usuario','email_usuario','password_usuario','activo_usuario','id_rol'];//Las columnas de la tabla
+    protected $useTimestamps = false; //Para evitar guardar y asignar fechas automaticamente
+    protected $returnType = 'object'; //Se especifica el tipo de dato a devolver
+
+
+    /*Se crea un método para obtener la información de un usuario.
+    Se piensa que sea utilizada para mostrar en la pantalla principal en algún lado.
+    También puede ser utilizada posteriormente para mostrar la información de usuarios
+    que hayan realizado pedidos y aún estén registrados en el sistema (activos).
+    */
+    public function obtenerInfoUsuario(int $id): array
+    {
+        $builder = $this->db->table('Usuario u');//Consulta a la BD.
+        $builder->select(
+            'u.id_usuario, u.dni_usuario,
+            CONCAT (u.apellido_usuario, + ,u.nombre_usuario) AS nombre_completo,
+            u.email_usuario, r.id_rol, r.nombre_rol');
+        $builder->join('Rol r', 'r.id_rol = u.id_rol');//Se hace el JOIN con la tabla Rol para obtener el id del tipo de rol.
+        $builder->where('u.id_usuario', $id);//Se filtra por el ID del usuario.
+        $builder->where('u.activo_usuario', 1);//Se filtra por usuarios activos.
+        //No se si vamos a buscar a los usuarios por su nombre completo, dni, email o que. Le dejo ID como default
+
+        //Se obtienen los usuarios activos y su información
+        return $builder->get()->getResult();
+    }
+}
