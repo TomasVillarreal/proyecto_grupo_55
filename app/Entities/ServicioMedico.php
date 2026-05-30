132bf550d532755ffcfb0af577243ac5677dfc2b
Tomas Villarreal <tomiavillarreal@gmail.com>
2026-05-30 12:41:02 -0300

    creacion de las entidades de estado pedido, proveedor y servicio medico (son todos copias y pegas de entidades existentes)

---
+++ app/Entities/ServicioMedico.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entities;
+
+class ServicioMedico 
+{
+    private int $id;
+    private string $nombre;
+
+    public function __construct(int $id, string $nombre){
+        $this->asignarID($id);
+        $this->cambiarNombre($nombre);
+    }
+
+    private function asignarID(int $id) : void
+    {
+        $this->id = $id;
+    }
+
+    public function cambiarNombre(string $nombre) : void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function obtenerID() : int
+    {
+        return $this->id;
+    }
+
+    public function obtenerNombre() : string
+    {
+        return $this->nombre;
+    }
+}
