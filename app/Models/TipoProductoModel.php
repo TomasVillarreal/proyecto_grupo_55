c30553b56c569c48b9f01245abd303f5f3df12a8
Tomas Villarreal <tomiavillarreal@gmail.com>
2026-05-30 14:38:14 -0300

    adaptacion del model de tipoproducto, donde ahora unicamente tendra la funcion de obtenerTodos y no hago aun lo del dropdown

--- app/Models/TipoProductoModel.php
+++ app/Models/TipoProductoModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Entities\TipoProducto;
 
 class TipoProductoModel extends Model
 {
@@ -10,21 +11,44 @@ class TipoProductoModel extends Model
     protected $primaryKey = 'id_tipo_producto'; //Identificador único
     protected $allowedFields = ['nombre_tipo_producto']; //La columna de la tabla
     protected $useTimestamps = false; //Para evitar asignaciones y guardados de fecha automaticamente.
-    protected $returnType = 'object'; //Formato de dato a devolver
+
+    private ?array $cacheTiposProducto = null;
+
+    public function obtenerTodos(): array
+    {
+        if ($this->cacheTiposProducto !== null) {
+            return $this->cacheTiposProducto;
+        }
+
+        $registros = $this->orderBy('nombre_tipo_producto', 'ASC')->findAll();
+
+        $tipos = [];
+
+        foreach ($registros as $registro) {
+            $tipos[] = new TipoProducto(
+                $registro['id_tipo_producto'],
+                $registro['nombre_tipo_producto']
+            );
+        }
+
+        $this->cacheTiposProducto = $tipos;
+
+        return $this->cacheTiposProducto;
+    }
 
     /*Se crea un método para obtener todos los tipos de productos ordenados alfabeticamente,
     lo cual va a ser útil para cargar en el formulario.
     Asocia a cada ID su tipo de producto y es más fácil llamarlo en los formualrios
-    ahorrando lógica en un controlador*/
-
+    ahorrando lógica en un controlador
     public function obtenerParaDropdown(): array
     {
-        $tipos = $this->orderBy('nombre_tipo_producto', 'ASC')->findAll();
-        $opciones_tipos = [];
-        foreach ($tipos as $tipo) {
-            $opciones_tipos[$tipo->id_tipo_producto] = $tipo->nombre_tipo_producto;
+        $opciones = [];
+
+        foreach ($this->obtenerTodos() as $tipoProducto) {
+            $opciones[$tipoProducto->obtenerID()] = $tipoProducto->obtenerNombre();
         }
-        return $opciones_tipos;
-    }
+
+        return
+    }*/
 }
 
