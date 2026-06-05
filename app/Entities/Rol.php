a0f47e2356f800be4c8415b42aa8bffcc5632ac9
Juan Cruz San Lorenzo <sanlorenzojuancruz@gmail.com>
2026-06-05 14:05:06 -0300

    Se creo este archivo para la entidad de rol

---
+++ app/Entities/Rol.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entities;
+
+class Rol 
+{
+    private int $id;
+    private string $nombre;
+
+    public function __construct(int $id, string $nombre){
+        $this->asignarID($id);
+        $this->asignarNombre($nombre);
+    }
+
+    private function asignarID(int $id) : void
+    {
+        $this->id = $id;
+    }
+
+    private function asignarNombre(string $nombre) : void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function obtenerID() : int
+    {
+        return $this->id;
+    }
+
+    public function obtenerNombre() : string
+    {
+        return $this->nombre;
+    }
+}
